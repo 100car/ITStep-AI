f2ba234a5a19afe5a1dbc908846da2327f2fb200
Serhii <100car@users.noreply.github.com>
2026-01-28 21:14:40 +0200

    Лейкемія.

--- classwork_2026_01_28+.py
+++ classwork_2026_01_28+.py
@@ -1,8 +1,9 @@
 import onnxruntime as ort
+import torch
+from torch.sparse import softmax
 from torchvision import transforms
 from PIL import Image
 import numpy as np
-
 
 test_transform = transforms.Compose(
     [transforms.Resize([200, 200]),
@@ -12,14 +13,42 @@ test_transform = transforms.Compose(
 )
 
 class_names = ['all', 'hem']
+names_codes = ['хвора', 'здорова']
 
 session = ort.InferenceSession("leukemia.onnx")
 
-image = Image.open("data/lesson many/cells/UID_H13_11_5_hem.bmp")
+image = Image.open("data/lesson many/cells/UID_H13_15_3_hem.bmp")
 # image.show()
 
-input_tensor = test_transform(image)
-print(input_tensor, input_tensor.shape)
+input_tensor: torch.Tensor = test_transform(image)
+input_tensor = input_tensor.unsqueeze(0)
+
+input_tensor = input_tensor.numpy()
+
+results = session.run(
+    None,
+    input_feed={"input": input_tensor}
+)
+
+result = results[0][0]
+
+ind = result.argmax()
+name = class_names[ind]
+
+result_tensor = torch.tensor(result)
+softmax = torch.nn.Softmax()
+probs = softmax(result_tensor).numpy()
+prob = probs[ind]
+
+print(f"На цьому малюнку з ймовірністю {prob*100}% зображена клітина {name} ({names_codes[ind]})." )
+
+
+
+
+
+
+
+# print(input_tensor, input_tensor.shape)
 
 
 
